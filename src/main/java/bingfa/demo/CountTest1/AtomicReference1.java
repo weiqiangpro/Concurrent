8c31196228b906f84059a13bac3b6df124103e1a
魏小强 <280803590@qq.com>
2019-09-17 19:35:49 +0800

    2

---
+++ src/main/java/bingfa/demo/CountTest1/AtomicReference1.java
@@ -0,0 +1,20 @@
+package bingfa.demo.CountTest1;
+
+import bingfa.demo.annations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@Slf4j
+@ThreadSafe
+public class AtomicReference1 {
+    private static AtomicReference<Integer> atomicReference = new AtomicReference<>(0);
+
+    public static void main(String[] args) {
+        atomicReference.compareAndSet(0,2);
+        atomicReference.compareAndSet(0,1);
+        atomicReference.compareAndSet(1,3);
+        atomicReference.compareAndSet(2,4);
+         log.info("{}",atomicReference);
+    }
+}
