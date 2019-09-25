cea781e8d5156442cf0270e81748fbb500cd2704
weiqiangpro <280803590@qq.com>
2019-09-25 10:13:59 +0800

    1

---
+++ src/main/java/bingfa/demo/StringTest/StringExample2.java
@@ -0,0 +1,45 @@
+package bingfa.demo.StringTest;
+
+import bingfa.demo.annations.NotThreadSafe;
+import bingfa.demo.annations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@ThreadSafe
+public class StringExample2 {
+    private final static Object o = new Object();
+    public static int clienttotal = 5000;
+    public static  int threadtotal = 200;
+    public static StringBuffer str=new StringBuffer("");
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadtotal);
+
+        final CountDownLatch  countDownLatch  = new CountDownLatch(clienttotal);
+        for (int i = 0 ; i < clienttotal;i++ ) {
+                executorService.execute(() -> {
+                    try {
+                        semaphore.acquire();
+                        add();
+                        semaphore.release();
+                    } catch (Exception e) {
+                        log.error("exception", e);
+                    }
+                    countDownLatch.countDown();
+                });
+            }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}",str.length());
+
+    }
+    public static  void  add(){
+        str.append("1");
+    }
+}
